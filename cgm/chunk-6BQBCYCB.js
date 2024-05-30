894d4e1cefd45dbbc767b0eb3c0b96b85f35648a
karim somai <somaikarim@gmail.com>
2024-05-30 14:27:03 +0200

    cgm

---
+++ cgm/chunk-6BQBCYCB.js
@@ -0,0 +1,1 @@
+var t={production:!0,ApiUrl:"https://api.github.com/"};var r=function(o){return o.Repos="search/repositories",o.Commits="repositories/repoID/commits",o}(r||{});export{t as a,r as b};
